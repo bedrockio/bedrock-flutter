9d33e830ac447734f6d0fc9e9c0309c1dd201541
Andrew Gabriel <Andrew@andrewtechful.com>
2021-10-28 02:51:13 -0400

    Update HomeView to add support for authentication screen for non logged-in users

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,8 +1,10 @@
+import 'package:bedrock_flutter/src/auth/auth_controller.dart';
+import 'package:bedrock_flutter/src/auth/auth_view.dart';
+import 'package:bedrock_flutter/src/widgets/bottom_navigation/bottom_navigation_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/bottom_navigation/bottom_navigation_controller.dart';
-import '../widgets/bottom_navigation/bottom_navigation_view.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,14 +17,35 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: _setAppBarTitle(context),
-      ),
-      body: Consumer<BottomNavigationController>(
-        builder: (context, controller, child) => controller.body,
-      ),
-      bottomNavigationBar: const BottomNavigation(),
+    final authController = Provider.of<AuthController>(context);
+
+    return FutureBuilder(
+      future: authController.authenticated,
+      builder: (context, AsyncSnapshot<bool> snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: false,
+              title: _setAppBarTitle(context),
+              actions: [
+                TextButton(
+                  onPressed: () => authController.logout(),
+                  child: const Text(
+                    'Logout',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            body: Consumer<BottomNavigationController>(
+              builder: (context, controller, child) => controller.body,
+            ),
+            bottomNavigationBar: const BottomNavigation(),
+          );
+        } else {
+          return const AuthView();
+        }
+      },
     );
   }
 }
