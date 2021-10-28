d9b9c050d149218523ca460217b7221ac5b6b967
Andrew Gabriel <Andrew@andrewtechful.com>
2021-10-28 02:52:21 -0400

    Update BottomNavigationBar to include products tab

--- lib/src/widgets/bottom_navigation/bottom_navigation_controller.dart
+++ lib/src/widgets/bottom_navigation/bottom_navigation_controller.dart
@@ -1,4 +1,8 @@
+import 'package:bedrock_flutter/src/products/products_controller.dart';
+import 'package:bedrock_flutter/src/products/products_list_view.dart';
+import 'package:bedrock_flutter/src/shops/shops_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../settings/settings_view.dart';
 import '../../shops/shop_list_view.dart';
@@ -16,6 +20,8 @@ class BottomNavigationController extends ChangeNotifier {
       case 0:
         return ShopListView.appBarLabel;
       case 1:
+        return ProductsListView.appBarLabel;
+      case 2:
         return SettingsView.appBarLabel;
       default:
         return 'Home';
@@ -25,8 +31,16 @@ class BottomNavigationController extends ChangeNotifier {
   Widget get body {
     switch (selectedTab) {
       case 0:
-        return Container();
+        return ChangeNotifierProvider<ShopsController>(
+          create: (BuildContext context) => ShopsController(),
+          child: const ShopListView(),
+        );
       case 1:
+        return ChangeNotifierProvider<ProductsController>(
+          create: (BuildContext context) => ProductsController(),
+          child: const ProductsListView(),
+        );
+      case 2:
         return const SettingsView();
       default:
         return Container();
--- lib/src/widgets/bottom_navigation/bottom_navigation_view.dart
+++ lib/src/widgets/bottom_navigation/bottom_navigation_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './bottom_navigation_controller.dart';
+import '../../products/products_list_view.dart';
 import '../../settings/settings_view.dart';
 import '../../shops/shop_list_view.dart';
 
@@ -19,6 +20,10 @@ class BottomNavigation extends StatelessWidget {
             BottomNavigationBarItem(
               icon: Icon(Icons.storefront),
               label: ShopListView.appBarLabel,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_grocery_store_rounded),
+              label: ProductsListView.appBarLabel,
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.settings),
