ec7cf5074fe384d1e2975ea3025c8ac7bfba52f7
Andrew Gabriel <Andrew@andrewtechful.com>
2021-10-29 18:06:25 -0400

    Refactor HomeView to show/hide Authentication given all variations of authentication state

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -1,10 +1,11 @@
-import 'package:bedrock_flutter/src/auth/auth_controller.dart';
-import 'package:bedrock_flutter/src/auth/auth_view.dart';
-import 'package:bedrock_flutter/src/widgets/bottom_navigation/bottom_navigation_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../auth/auth_controller.dart';
+import '../auth/auth_view.dart';
 import '../widgets/bottom_navigation/bottom_navigation_controller.dart';
+import '../widgets/bottom_navigation/bottom_navigation_view.dart';
+import '../widgets/loading_screen.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -22,28 +23,37 @@ class Home extends StatelessWidget {
     return FutureBuilder(
       future: authController.authenticated,
       builder: (context, AsyncSnapshot<bool> snapshot) {
-        if (snapshot.hasData) {
-          return Scaffold(
-            appBar: AppBar(
-              centerTitle: false,
-              title: _setAppBarTitle(context),
-              actions: [
-                TextButton(
-                  onPressed: () => authController.logout(),
-                  child: const Text(
-                    'Logout',
-                    style: TextStyle(color: Colors.white),
-                  ),
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return const AuthView();
+          case ConnectionState.waiting:
+            return const LoadingScreen();
+          case ConnectionState.active:
+            return const AuthView();
+          case ConnectionState.done:
+            if (snapshot.data == true) {
+              return Scaffold(
+                appBar: AppBar(
+                  centerTitle: false,
+                  title: _setAppBarTitle(context),
+                  actions: [
+                    TextButton(
+                      onPressed: () => authController.logout(),
+                      child: const Text(
+                        'Logout',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            body: Consumer<BottomNavigationController>(
-              builder: (context, controller, child) => controller.body,
-            ),
-            bottomNavigationBar: const BottomNavigation(),
-          );
-        } else {
-          return const AuthView();
+                body: Consumer<BottomNavigationController>(
+                  builder: (context, controller, child) => controller.body,
+                ),
+                bottomNavigationBar: const BottomNavigation(),
+              );
+            } else {
+              return const AuthView();
+            }
         }
       },
     );
