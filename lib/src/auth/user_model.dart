5198ccccb858ac0deb94fb1f864e304dd27a0a8d
Andrew Gabriel <Andrew@andrewtechful.com>
2021-11-04 00:11:19 -0400

    Add getters to User model for network request params

--- lib/src/auth/user_model.dart
+++ lib/src/auth/user_model.dart
@@ -10,4 +10,19 @@ class User {
     this.firstName,
     this.lastName,
   });
+
+  Map<String, dynamic> get loginParams {
+    return {'email': email, 'password': password};
+  }
+
+  Map<String, dynamic> get registerParams {
+    return {
+      'email': email,
+      'firstName': firstName,
+      'lastName': lastName,
+      'password': password,
+    };
+  }
+
+  Map<String, dynamic> get resetPasswordParams => {'email': email};
 }
