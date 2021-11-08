f65863916e167e3c9b2e98d631c916999ee3f03a
Tiago Rocha <tiago.aziz@gmail.com>
2021-11-08 14:28:53 -0100

    removes unused import

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,4 @@
 import 'package:bedrock_flutter/src/route_generator.dart';
-import 'package:bedrock_flutter/src/shops/shop_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
