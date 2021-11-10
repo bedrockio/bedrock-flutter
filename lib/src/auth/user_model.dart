28f93bdc95079f981be2f9ef875f1239756451fb
Andrew Gabriel <Andrew@andrewtechful.com>
2021-11-09 23:17:31 -0500

    Update User Model to support attributes coming from API and generation JSON serialization accordingly

--- lib/src/auth/user_model.dart
+++ lib/src/auth/user_model.dart
@@ -1,28 +1,51 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class User {
   String email;
   String? firstName;
   String? lastName;
-  String password;
+  String? name;
+  String? password;
+  String? createdAt;
+  String? updatedAt;
+  String? id;
+  List<dynamic>? roles;
 
   User({
     required this.email,
-    required this.password,
+    this.password,
+    this.name,
     this.firstName,
     this.lastName,
+    this.createdAt,
+    this.updatedAt,
+    this.id,
+    this.roles,
   });
 
-  Map<String, dynamic> get loginParams {
-    return {'email': email, 'password': password};
-  }
+  Map<String, dynamic> get loginParams => {
+        'email': email,
+        'password': password,
+      };
 
-  Map<String, dynamic> get registerParams {
-    return {
-      'email': email,
-      'firstName': firstName,
-      'lastName': lastName,
-      'password': password,
-    };
-  }
+  Map<String, dynamic> get registerParams => {
+        'email': email,
+        'firstName': firstName,
+        'lastName': lastName,
+        'password': password,
+      };
 
   Map<String, dynamic> get resetPasswordParams => {'email': email};
+
+  Map<String, dynamic> get updateParams => {
+        'firstName': firstName ?? '',
+        'lastName': lastName ?? '',
+      };
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
